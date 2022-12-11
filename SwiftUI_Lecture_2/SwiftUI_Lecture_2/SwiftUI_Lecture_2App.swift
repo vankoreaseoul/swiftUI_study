2448997a97f8ace10f1c98ca783451dfa0f0238d
vankoreaseoul <gofltmxj@naver.com>
2022-12-11 15:41:16 +0900

    edit directory hierarchy

---
+++ SwiftUI_Lecture_2/SwiftUI_Lecture_2/SwiftUI_Lecture_2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Lecture_2App.swift
+//  SwiftUI_Lecture_2
+//
+//  Created by Heawon Seo on 2022/12/11.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Lecture_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
